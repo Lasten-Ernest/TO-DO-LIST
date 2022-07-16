c15363cb22431dc261b13cc8ff5ac96f986e3664
Lasten-Ernest <lastenernest@gmail.com>
2022-07-16 12:45:51 +0200

    Update code in completedtasks.js

--- src/completedtasks.js
+++ src/completedtasks.js
@@ -3,7 +3,6 @@ const storetask = (todoList) => {
 };
 
 const gettasks = () => JSON.parse(localStorage.getItem('todo-list-group'));
-// *****Make checkbox true for completed tasks
 export default (e) => {
   const list = gettasks();
   if (e.target.checked === true) {
